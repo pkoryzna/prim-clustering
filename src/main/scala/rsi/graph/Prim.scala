96f2a7b398e202eb3310cd1d7f15a4e8f3b84162
Patryk Koryzna <pkoryzna@gmail.com>
2015-05-27 02:41:44 +0200

    refactored mst to have parametrized vertices to search, matrix type

--- src/main/scala/rsi/graph/Prim.scala
+++ src/main/scala/rsi/graph/Prim.scala
@@ -4,6 +4,8 @@ import scala.annotation.tailrec
 
 object Prim {
 
+  type Matrix = Int => Vector[Double]
+
   def mst(input: UndirectedGraph): UndirectedGraph = {
     val vertex = input.vertices.head
     val cheapestEdge = input.edges(vertex).minBy(_.weight)
@@ -15,17 +17,33 @@ object Prim {
   private def mst(inputGraph: UndirectedGraph, currentPartial: UndirectedGraph, vertices: Set[Int]): UndirectedGraph = {
     if (currentPartial.vertices == vertices) currentPartial
     else {
-      val verticesToSearch = currentPartial.vertices
-      // get all edges connected to vertices of partial mst
-      val reachable: Set[Edge] = verticesToSearch.flatMap(from => inputGraph.matrix(from).zipWithIndex.collect {
-        case (weight, to) if weight != Edge.UNREACHABLE => Edge(from, to, weight)
-      })
-      // filter edges to vertices already in partial mst
-      val candidates: Set[Edge] = reachable.filterNot { case Edge(from, to, _) => currentPartial.matrix(from)(to) != Edge.UNREACHABLE }
-      // add the cheapest to partial mst and recur
-      val cheapest: Edge = candidates.minBy(_.weight)
+      val verticesToSearch: Set[Int] = currentPartial.vertices
+      val matrix: Matrix = inputGraph.matrix
 
-      mst(inputGraph, currentPartial + cheapest, vertices)
+      // get all edges connected to vertices of partial mst
+      val reachable: Set[Edge] = findReachable(verticesToSearch.toSeq, matrix)
+      // filter edges to vertices already in partial mst
+      val candidates: Set[Edge] = notInCurrentMST(reachable, currentPartial)
+      // add the cheapest to partial mst and recur
+      val cheapestEdge: Edge = cheapest(candidates)
+
+      mst(inputGraph, currentPartial + cheapestEdge, vertices)
     }
+  }
+
+  def findReachable(verticesToSearch: Seq[Int], matrix: Int => Vector[Double]): Set[Edge] = {
+    verticesToSearch.flatMap(from => {
+      matrix(from).zipWithIndex.collect {
+        case (weight, to) if weight != Edge.UNREACHABLE => Edge(from, to, weight)
+      }
+    }).toSet
+  }
+
+  def notInCurrentMST(reachable: Set[Edge], partial: UndirectedGraph): Set[Edge] = {
+    reachable.filterNot { case Edge(from, to, _) => partial.matrix(from)(to) != Edge.UNREACHABLE }
+  }
+
+  def cheapest(candidates: Set[Edge]): Edge = {
+    candidates.minBy(_.weight)
   }
 }
